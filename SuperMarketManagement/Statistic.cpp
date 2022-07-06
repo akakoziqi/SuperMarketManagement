611be57596d7911801971a8899b03e4400fdd53f
Akako <69710421+akakoziqi@users.noreply.github.com>
2022-07-06 09:50:34 +0800

    添加开源协议

--- SuperMarketManagement/Statistic.cpp
+++ SuperMarketManagement/Statistic.cpp
@@ -1,4 +1,35 @@
-﻿// Statistic.cpp: 实现文件
+﻿/*
+ | 超市管理系统S
+ | 文件名称: Statistic.cpp
+ | 文件作用: 统计信息
+ | 创建日期: 2022-07-02
+ | 更新日期: 2022-07-05
+ | 开发人员: Akako
+ +----------------------------
+ MIT License
+
+ Copyright (C) Akako
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
+// Statistic.cpp: 实现文件
 //
 
 #include "pch.h"
